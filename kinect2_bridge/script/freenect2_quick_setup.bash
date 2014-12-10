5ade35b0edf7942d6e1480d0f9a08994681f73df
Paul Bovbel <pbovbel@clearpathrobotics.com>
2014-12-10 12:25:01 -0500

    RP-255 modify quick setup script

--- kinect2_bridge/script/freenect2_quick_setup.bash
+++ kinect2_bridge/script/freenect2_quick_setup.bash
@@ -1,19 +1,26 @@
-#dependencies
+# Pull freenect2 dependencies
 sudo apt-get install -y ros-indigo-perception
 sudo apt-get install -y nvidia-331-dev
 sudo apt-get install -y ocl-icd-opencl-dev
 sudo apt-get install -y libtool automake libudev-dev
 
+# Pull known-working freenect2 fork
 cd ~
 git clone https://github.com/clearpathrobotics/libfreenect2.git
 cd libfreenect2/depends
 sh install_ubuntu.sh
 
+# Fix turbonect
 sudo ln -s /usr/lib/x86_64-linux-gnu/libturbojpeg.so.0.0.0 /usr/lib/x86_64-linux-gnu/libturbojpeg.so
 
+# Compile freenect2 prototype
 cd ../examples/protonect
 cmake CMakeLists.txt
 make
 sudo make install
 
+# Remove existing entries
+sed -i "/\b\(LD_LIBRARY_PATH\)\b/d" ~/.bashrc
+
+# Append to LD_LIBRARY_PATH
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/:/usr/local/./lib64/' >> ~/.bashrc
